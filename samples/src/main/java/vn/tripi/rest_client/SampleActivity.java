918708d3732869a0a9547dd532be5ec50429b8f8
Spider man <nguyendo94vn@gmail.com>
2018-03-31 11:06:55 +0700

    add file samples

---
+++ samples/src/main/java/vn/tripi/rest_client/SampleActivity.java
@@ -0,0 +1,14 @@
+package vn.tripi.rest_client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+    }
+}
